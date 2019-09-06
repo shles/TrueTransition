d92db6e496f78c7d66dc8cea5202a929640f6a40
Booharin <booharin@bk.ru>
2019-09-06 18:16:57 +0300

    Fixed Ok button

--- Source/Transition.swift
+++ Source/Transition.swift
@@ -103,7 +103,7 @@ open class DefaultErrorAlertTransition: Transition {
     
     public func perform(on vc: UIViewController) {
         let alertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
-        alertController.addAction(UIAlertAction(title: "Ok", style: .default, handler: nil))
+        alertController.addAction(UIAlertAction(title: okActionTitle, style: .default, handler: nil))
         vc.present(alertController, animated: true, completion: nil)
     }
     
