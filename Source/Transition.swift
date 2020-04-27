4ed2fe14bea04e6ee8e31adfa24b631d3937e931
Artemiy Shlesberg <temitrix@gmail.com>
2020-04-28 00:34:49 +0300

    Remove card Transition

--- Source/Transition.swift
+++ Source/Transition.swift
@@ -10,15 +10,6 @@ public protocol Transition {
     func perform(on vc: UIViewController)
 }
 
-public protocol CustomAnimation {
-    func perform(on vc: UIViewController,
-                 durationIn: Double,
-                 velocityIn: CGFloat,
-                 dampingIn: CGFloat,
-                 durationOut: Double,
-                 velocityOut: CGFloat,
-                 dampingOut: CGFloat)
-}
 
 //TODO: make options to present non animated or smth
 
@@ -94,59 +85,6 @@ open class  NewWindowRootControllerTransition: Transition {
         }
     }
 }
-
-open class CardsTransition: NSObject, Transition {
-    
-    var durationIn: Double
-    var velocityIn: CGFloat
-    var dampingIn: CGFloat
-    var durationOut: Double
-    var velocityOut: CGFloat
-    var dampingOut: CGFloat
-    
-    
-    private var controllerToPresent: () -> (UIViewController & CardContentControllerProtocol)
-    
-    public init(controllerToPresent: @escaping () -> (UIViewController & CardContentControllerProtocol),
-                durationIn: Double = 0.3,
-                velocityIn: CGFloat = 0.1,
-                dampingIn: CGFloat = 0.8,
-                durationOut: Double = 0.3,
-                velocityOut: CGFloat = 0.1,
-                dampingOut: CGFloat = 0.8)  {
-        
-        self.durationIn = durationIn
-        self.velocityIn = velocityIn
-        self.dampingIn = dampingIn
-        self.durationOut = durationOut
-        self.velocityOut = velocityOut
-        self.dampingOut = dampingOut
-        
-        self.controllerToPresent = controllerToPresent
-    }
-    
-    public func perform(on vc: UIViewController) {
-        
-        let cardViewController = CardsViewController(nibName: "CardsViewController",
-                                                     bundle: Bundle(for: self.classForCoder))
-        let controller = controllerToPresent()
-        controller.modalPresentationStyle = .overCurrentContext
-        cardViewController.backingImage = UIApplication.shared.keyWindow?.makeSnapshot()
-        cardViewController.controllerToPresent = controller
-        
-        //animationParameters
-        cardViewController.durationIn = durationIn
-        cardViewController.velocityIn = velocityIn
-        cardViewController.dampingIn = dampingIn
-        
-        cardViewController.durationOut = durationOut
-        cardViewController.velocityOut = velocityOut
-        cardViewController.dampingOut = dampingOut
-        
-        vc.present(cardViewController, animated: false)
-    }
-}
-
 
 open class AlertTransition: Transition {
     
